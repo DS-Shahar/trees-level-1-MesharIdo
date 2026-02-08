6376220332e0790ec2c8eafa3a44354476952131
MesharIdo <idoly.meshar@gmail.com>
2026-02-08 10:45:31 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    
   }
+
+    public static boolean ex_22(BinNode<Integer> t)
+    {
+        if (t==null)
+            return true;
+            
+        if (!t.hasRight() && t.hasLeft() || t.hasRight() && !t.hasLeft())
+            return false;
+            
+        return ex_22(t.getLeft()) && ex_22(t.getRight())
+    }
 }
